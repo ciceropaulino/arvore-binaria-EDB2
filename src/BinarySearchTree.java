a847c15c1e099ea532175b30c28c6be8cd4f1a66
Cicero Paulino <cicero@pop-os.localdomain>
2023-05-26 15:55:50 -0300

    Implementação de todos os metodos exceto o de printar de acordo como requisitado.

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -2,13 +2,13 @@
 public class BinarySearchTree {
 
     Node root;
-
-    public void insert(int value) { //front method to insert
+    /*Metodo de usuario para inserir.*/
+    public void insert(int value) {
 
         Node node = new Node(value);
         root = backInsert(root, node);
     }
-
+    /*Metodo de insersao recursivo privado*/
     private Node backInsert(Node root, Node node) {
 
         if (root == null) {
@@ -38,15 +38,15 @@ public class BinarySearchTree {
 
         }
     }
-
-    public boolean search(int value) { //Front method to search
+    /*Metodo de usuario para buscar*/
+    public boolean search(int value) {
 
         int data = value;
 
         return (backSearch(root, data))?
          true : false;
     }
-
+    /*Metodo de busca recursivo privado*/
     private boolean backSearch(Node root, int data) {
         if(root == null) {
             return false;
@@ -58,7 +58,7 @@ public class BinarySearchTree {
             return backSearch(root.right, data);
         }
     }
-
+    /*Metodo de usuario para remover no da arvore*/
     public void remove(int data) {
         if (search(data)) {
             root = backRemove(root, data);
@@ -66,7 +66,7 @@ public class BinarySearchTree {
             System.out.println(data + " Remove error, root not found.");
         }
     }
-
+    /*Metodo de remorcao recursivo privado*/
     private Node backRemove(Node root, int data) {
         if (root == null) {
             return null;
@@ -90,7 +90,7 @@ public class BinarySearchTree {
     
         return root;
     }
-
+    /*Metodo retorna o sucessor de uma determinada sub-raiz*/
     private int successor(Node root) {
         root = root.right;
         while (root.left != null) {
@@ -98,7 +98,7 @@ public class BinarySearchTree {
         }
         return root.data;
     }
-
+    /*Metodo retorna o predecessor de uma determinada sub-raiz*/
     private int predecessor(Node root) {
         root = root.left;
         while (root.right != null) {
@@ -108,7 +108,9 @@ public class BinarySearchTree {
     }
 
     //#########################################################
-
+    /*Metodo de usuario que recebe uma posicao inteira e retorna o numero correspondente a essa po-
+    sicao
+     */
     public int nElement(int value) {
         Counter count = new Counter();
         Node element = searchSymmetricOrder(root, value, count);
@@ -145,7 +147,7 @@ public class BinarySearchTree {
         return s;
     }
 
-    public int backPosition(Node root, int value, Counter count) {
+    private int backPosition(Node root, int value, Counter count) {
         if (root != null) {
             int posicaoEsquerda = backPosition(root.left, value, count);
             if (posicaoEsquerda != -1) {
@@ -162,50 +164,45 @@ public class BinarySearchTree {
         return -1; 
     }
 
-    public Node searchForMedia(Node root, int key) { /// seria melhor que fosse so um boleano para ver se existe e rodar.
-        if (root == null || root.data == key){
-            return root;
-        }
-
-        if (key < root.data){
-            return searchForMedia(root.left, key);
-        }
-
-        return searchForMedia(root.right, key);
-    }
-
+    /*metodo de usuario que retorna a media dos elementos da arvore*/
     public double media (int x){
-        Node temp = searchForMedia(root, x);
-        Counter count = new Counter();
-        double soma = countElements(temp, count, 1);
-        count.count *= 0;
-        double numElements = countElements(temp, count, 0);
-        return soma/numElements;
+        if(search(x)) {
+            Node temp = new Node(x);
+            Counter count = new Counter();
+            double soma = countElements(temp, count, 1);
+            count.count *= 0;
+            double numElements = countElements(temp, count, 0);
+            return soma/numElements;
+        } else {
+            System.out.println("ERROR: root invalid.");
+            return -1;
+        }
+
     }
-    
-    public int mediana(){
+    /*Metodo de usuario que retorna a mediana dos elementos da arvore*/
+    public int median(){
         Counter count = new Counter();
         double numElements = countElements(root, count, 0);
         
-        return medianaRec(root, numElements);
+        return backMedian(root, numElements);
     }
-    
-    public int medianaRec(Node root, double numElements){
+    /*Metodo auxiliar recursivo que busca pela mediana*/
+    private int backMedian(Node root, double numElements){
         int p = position(root.data);
         if(p == Math.ceil(numElements/2)){
             return root.data;
         }
         else{
             if(p < Math.ceil(numElements/2)){
-                return medianaRec(root.right, numElements);
+                return backMedian(root.right, numElements);
             }
             else{
-                return medianaRec(root.left, numElements);
+                return backMedian(root.left, numElements);
             }
         }
     }
-    
-    public double countElements(Node root, Counter count, int x){
+    /*Metodo contador de elementos*/
+    private double countElements(Node root, Counter count, int x){
         if(root != null){
             if(x == 1){
                 count.count+= root.data;
@@ -223,18 +220,61 @@ public class BinarySearchTree {
         
         return (double)count.count;
     }
-    
-    public String pre_ordem(){
+    /*Metodo de usuario que retorna string dos elementos da arvore em pre-ordem*/
+    public String preOrder(){
         StringBuilder sb = new StringBuilder();
-        preOrdemRec(root, sb);
+        backPreOrder(root, sb);
         return sb.toString();
     }
-    
-    public void preOrdemRec(Node root, StringBuilder sb){
+    /*Metodo auxiliar recursivo que percorre a arvore em preordem */ 
+    private void backPreOrder(Node root, StringBuilder sb){
         if(root != null){
             sb.append(root.data).append(" ");
-            preOrdemRec(root.left, sb);
-            preOrdemRec(root.right, sb);
+            backPreOrder(root.left, sb);
+            backPreOrder(root.right, sb);
         }
     }
+    //###########################################################//
+    //####################################
+    public boolean isFull() {
+        return backIsFull(root);
+    }
+
+    private boolean backIsFull(Node node) {
+        if (node == null)
+            return true;
+
+        if (node.left == null && node.right == null)
+            return true;
+
+        if (node.left != null && node.right != null)
+            return (backIsFull(node.left) && backIsFull(node.right));
+
+        return false;
+    }
+
+    public boolean isComplete() {
+        int index = 0;
+        int count = countNodes(root);
+        return backIsComplete(root, index, count);
+    }
+    
+    private int countNodes(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + countNodes(node.left) + countNodes(node.right);
+    }
+    
+    private boolean backIsComplete(Node node, int index, int count) {
+        if (node == null) {
+            return true;
+        }
+        if (index >= count) {
+            return false;
+        }
+        return backIsComplete(node.left, 2 * index + 1, count) &&
+               backIsComplete(node.right, 2 * index + 2, count);
+    }
+    
 }
--- src/TesterTree.java
+++ src/TesterTree.java
@@ -1,49 +1,30 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class TesterTree {
     public static void main(String[] args) {
 
-
-        File f = new File("numeros.txt");
         BinarySearchTree tree = new BinarySearchTree();
-        try{
-            Scanner scanner = new Scanner(f);
-            while(scanner.hasNext()){
-                if(scanner.hasNextInt()){
-                    int num = scanner.nextInt();
-                    tree.insert(num);    
-                }else{
-                    scanner.next();
-                }
-            }
-            scanner.close();
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-        }
 
+        tree.insert(4);
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(1);
         tree.insert(3);
         tree.insert(5);
-        tree.insert(6);
-        tree.insert(8);
-        tree.insert(9);
-
-        //tree.remove(2);
-        //tree.remove(90 );
-        tree.display();
-
-        System.out.println(tree.search(5));
-
-        //int position = tree.nElement(5);
-
-        //System.out.println("N element: " + position);
+        tree.insert(7);
 
         System.out.println("Enesimo Elemento: " + tree.nElement(3));
-        System.out.println("Posição do Elemento: " + tree.position(50));
-        System.out.println("Mediana da Árvore: " + tree.mediana());
+        System.out.println("Posição do Elemento: " + tree.position(6));
+        System.out.println("Mediana da Árvore: " + tree.median());
         System.out.printf("Media aritmética do nó: %.2f \n", tree.media(5));
-        System.out.println("Árvore em pré-ordem: " + tree.pre_ordem());
+        System.out.println("Árvore em pré-ordem: " + tree.preOrder());
+        if (tree.isFull())
+            System.out.println("A árvore binária é cheia");
+        else
+            System.out.println("A árvore binária não é cheia");
+
+        if (tree.isComplete())
+            System.out.println("A árvore binária é completa");
+        else
+            System.out.println("A árvore binária não é completa");
 
     }
 }
